598d7a93b695c8157296a6abf9486cfe92b4c192
Kazimierz Roman <kazimierz.roman.stud@pw.edu.pl>
2023-11-27 20:55:01 +0100

    smietnik

--- src/main.c
+++ src/main.c
@@ -16,18 +16,18 @@ typedef enum status{
 }status_t;
 
 status_t sensor_init(void){
-
-	
 	return OK;
 }
+
 /* change this to any other UART peripheral if desired */
 #define UART_DEVICE_NODE DT_CHOSEN(zephyr_shell_uart)
-#define UART1_NODE DT_ALIAS(arduino_serial)
+// #define UART1_NODE DT_NODELABEL(arduino_serial)
+#define UART1_NODE DT_NODELABEL(uart1)
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
 
-#define MSG_SIZE 32
+#define MSG_SIZE 22
 
 /* queue to store up to 10 messages (aligned to 4-byte boundary) */
 K_MSGQ_DEFINE(uart_msgq, MSG_SIZE, 10, 4);
@@ -42,7 +42,8 @@ K_MSGQ_DEFINE(uart_msgq, MSG_SIZE, 10, 4);
 #define PRIORITY 7
 
 static const struct device *const uart_dev = DEVICE_DT_GET(UART_DEVICE_NODE);
-static const struct device *const uart_debug = DEVICE_DT_GET(UART1_NODE);
+
+static const struct device *const uart_sensor = DEVICE_DT_GET(UART1_NODE);
 /* receive buffer used in UART ISR callback */
 static char rx_buf[MSG_SIZE];
 static int rx_buf_pos;
@@ -57,13 +58,13 @@ void serial_cb(const struct device *dev, void *user_data)
 {
 	uint8_t c;
 
-	if (!uart_irq_update(uart_dev)) {
+	if (!uart_irq_update(uart_sensor)) {
 		return;
 	}
 
-	while (uart_irq_rx_ready(uart_dev)) {
+	while (uart_irq_rx_ready(uart_sensor)) {
 
-		uart_fifo_read(uart_dev, &c, 1);
+		uart_fifo_read(uart_sensor, &c, 1);
 
 		rx_buf[rx_buf_pos++] = c;
 
@@ -75,14 +76,12 @@ void serial_cb(const struct device *dev, void *user_data)
 }
 
 /*
- * Print a null-terminated string character by character to the UART interface
+ * send a null-terminated string character by character to the UART interface
  */
-void print_uart(char *buf)
+void send_uart(char *buf, int n)
 {
-	int msg_len = strlen(buf);
-
-	for (int i = 0; i < msg_len; i++) {
-		uart_poll_out(uart_dev, buf[i]);
+	for (int i = 0; i < n; i++) {
+		uart_poll_out(uart_sensor, buf[i]);
 	}
 }
 
@@ -94,6 +93,7 @@ void print_basic_sensor_info(void){
 }
 K_THREAD_DEFINE(print_basic_sensor_info_id, STACKSIZE, print_basic_sensor_info, NULL, NULL, NULL,
 		PRIORITY, 0, 0);
+
 
 void main(void)
 {
@@ -112,7 +112,6 @@ void main(void)
 	/* ------------------------------------------------------------------------------------------------------------------ */
 	/*                                                        UART                                                        */
 	/* ------------------------------------------------------------------------------------------------------------------ */
-
 	char tx_buf[MSG_SIZE];
 
 	if (!device_is_ready(uart_dev)) {
@@ -120,19 +119,33 @@ void main(void)
 		return;
 	}
 
-	/* configure interrupt and callback to receive data */
-	uart_irq_callback_user_data_set(uart_dev, serial_cb, NULL);
-	uart_irq_rx_enable(uart_dev);
+	if (!device_is_ready(uart_sensor)) {
+		printk("UART device not found!");
+		return;
+	} else {
+		printk("UART_dbg device found!");
+	}
 
-	print_uart("Hello! I'm your echo bot.\r\n");
-	print_uart("Tell me something and press enter:\r\n");
+	/* configure interrupt and callback to receive data */
+	uart_irq_callback_user_data_set(uart_sensor, serial_cb, NULL);
+	uart_irq_rx_enable(uart_sensor);
+
+	uint8_t command[] = {0xF1, 0x1F, 0xE2, 0x2E, 0xB6, 0x6B, 0xA8, 0x8A, 0x00, 0x07, 0x86, 0x00, 0x00, 0x00, 0x00, 0x03, 0x03, 0xFA};
+	send_uart(command, sizeof(command));
 
 	/* indefinitely wait for input from the user */
-	while (k_msgq_get(&uart_msgq, &tx_buf, K_MSEC(200)) == 0) {
-		// print_uart("Echo: ");
-		// print_uart(tx_buf);
-		// print_uart("\r\n");
-	}
+	// while (k_msgq_get(&uart_msgq, &tx_buf, K_MSEC(200)) == 0) {
+	// 	// print_uart("Echo: ");
+	// 	// print_uart(tx_buf);
+	// 	// print_uart("\r\n");
+	// }
+	k_msgq_get(&uart_msgq, &tx_buf, K_MSEC(200));
+	printk("%s\n", tx_buf);
+	k_msgq_get(&uart_msgq, &tx_buf, K_MSEC(200));
+	printk("%s\n", tx_buf);
+	k_msgq_get(&uart_msgq, &tx_buf, K_MSEC(200));
+	printk("%s\n", tx_buf);
+	k_msgq_get(&uart_msgq, &tx_buf, K_MSEC(200));
 	printk("%s\n", tx_buf);
 
 	/* ------------------------------------------------------------------------------------------------------------------ */
